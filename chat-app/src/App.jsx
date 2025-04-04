de04a1d550e2553329643b41fca6c0218b0e8a2e
nagham <naghamhabli89@gmail.com>
2025-04-04 14:40:35 +0300

    default page

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
             }
           />
           
-          <Route path="/" element={<Navigate to="/chat" />} />
+          <Route path="/"  element={<Login />} />
         </Routes>
       </Router>
     </AuthProvider>
